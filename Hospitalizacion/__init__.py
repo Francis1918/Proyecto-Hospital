563cabc5f5726d69fadc35f079600bc0593889e1
Francis1918 <9alemanbravo@gmail.com>
2025-12-21 21:53:34 -0500

    Bugfixed

--- Hospitalizacion/__init__.py
+++ Hospitalizacion/__init__.py
@@ -1,3 +1,4 @@
-from .orden_view import GestionOrdenView
+from .gestion_orden.orden_view import GestionOrdenView
+from .hospitalizacion_view import HospitalizacionView
 
-__all__ = ["GestionOrdenView"]
+__all__ = ["GestionOrdenView", "HospitalizacionView"]
--- Hospitalizacion/gestion_orden/repository.py
+++ Hospitalizacion/gestion_orden/repository.py
@@ -1,9 +1,13 @@
+from .models import OrdenMedica
+
 class OrdenRepository:
     def __init__(self):
         self.ordenes = {}
 
-    def registrar_orden(self, orden: OrdenMedica):
+    def registrar(self, orden: OrdenMedica):
         self.ordenes.setdefault(orden.id_paciente, []).append(orden)
 
-    def obtener_ordenes(self, id_paciente: str):
+    def obtener_por_paciente(self, id_paciente: str):
         return self.ordenes.get(id_paciente, [])
+
+orden_repo = OrdenRepository()
